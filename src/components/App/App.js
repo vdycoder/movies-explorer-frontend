912bf7768bd89ffae71d7ecac3252de7a2c69cbb
Dmitry V <pub.dv@ya.ru>
2023-07-29 16:32:02 +0300

    add: register and login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,8 @@ import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
 import { checkRoute } from "../../utils/utils";
 
 import {
@@ -31,9 +33,9 @@ function App() {
           <Route path="/movies" element={<Movies />} />
           <Route path="/saved-movies" element={<SavedMovies />} />
           <Route path="/profile" element={<Profile />} />
-          {/* <Route path="/signup" element={<Register />} />
+          <Route path="/signup" element={<Register />} />
           <Route path="/signin" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} /> */}
+          {/*<Route path="*" element={<PageNotFound />} /> */}
         </Routes>
         {footerIsVisible && <Footer />}
       </div>
